9647d2c5ae3253f6f9675b6552cfc40996e1015c
LevinVinnuHusalie <levinjbi@gmail.com>
2023-11-27 22:48:48 +0700

    perbaikan homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,33 @@
-@extends('layouts.page')
-@section('mainContent')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/homepage.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Posts</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <div class="container-fluid">
+            <a class="navbar-brand h1 ms-4" href={{ route('posts.index') }}>
+                <img src="/MalangDrift.png">
+            </a>
+            <div class="container text-center row justify-content-center">
+                <div class="col ">
+                    <a class="btn btn-sm btn-info" href={{ route('posts.create') }}>Home</a>
+                    <a class="btn btn-sm btn-info" href={{ route('posts.create') }}>Cars</a>
+                    <a class="btn btn-sm btn-info" href={{ route('posts.create') }}>Location</a>
+                    <a class="btn btn-sm btn-info" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; --bs-btn-width: .75rem;" href={{ route('posts.create') }}>Your Order</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     <div class="container mt-5">
         <div class="row">
             <img src="/mobil.png" class="col-sm">
@@ -7,23 +35,19 @@
                 <p class="col-sm h1">Where Fast Cars <br> Meet Faster Lives</p>
                 <div class="row">
                     <div class="col-sm">
-                        <a class="btn btn-sm btn-info" href="">Choose Your Ride</a>
-                        <a class="btn btn-sm btn-dark" href="">Contact US</a>
+                        <a class="btn btn-sm btn-info" href={{ route('posts.create') }}>Choose Your Ride</a>
+                        <a class="btn btn-sm btn-dark" href={{ route('posts.create') }}>Contact US</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <div class="container mt-5">
         <div class="row">
             <img src="/step.png" class="col-sm">
         </div>
     </div>
-<<<<<<< HEAD
-    <div class=" mt-5">
-        <div class="row">
-            <img src="/Group.png" class="col-sm">
-=======
 
     <div class="container mt-5">
         <div class="row bg-image" style="background-image: url('bg.png');height: 50rem">
@@ -94,7 +118,9 @@
                             </div>
                         </div>
                     </div>
-
+                    <!-- <div class="" style="width: 20rem;">
+                        <a href="#" class="btn btn-primary mt-2" style="width: 5rem;">Go somewhere</a>
+                    </div> -->
                 </div>
                 <div class="container text-center row justify-content-center">
                     <div class="col ">
@@ -102,12 +128,15 @@
                     </div>
                 </div>
             </div>
->>>>>>> 3a25882 (perbaikan homepage)
         </div>
+
     </div>
+
     <div class=" mt-5">
         <div class="row">
             <img src="/foot.png" class="col-sm">
         </div>
     </div>
-@endsection
+</body>
+
+</html>
